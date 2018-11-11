0417eecb117bfdcf7610dbb493428bd8147b9bb6
Leighton villanueva <lavillan@hawaii.edu>
2018-11-11 09:36:07 -1000

    MusicLanding created and App updated to reflect new Landing Page

---
+++ app/imports/ui/pages/MusicLanding.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+
+/** A simple static component to render some text for the landing page. */
+class Landing extends React.Component {
+  render() {
+    return (
+        <Grid verticalAlign='middle' textAlign='center' container>
+
+          <Grid.Column width={4}>
+            <Image size='small' circular src="/images/meteor-logo.png"/>
+          </Grid.Column>
+
+          <Grid.Column width={8}>
+            <h1>Welcome to this template</h1>
+          </Grid.Column>
+
+        </Grid>
+    );
+  }
+}
+
+export default Landing;

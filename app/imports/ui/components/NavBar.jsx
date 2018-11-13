f80808a5a49908deec33c1b77d89e466a9ef35fc
Mia N Clause <mnclause@gmail.com>
2018-11-13 13:03:34 -1000

    NavBar (2)

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -9,7 +9,7 @@ import { Roles } from 'meteor/alanning:roles';
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px' };
+    const menuStyle = { marginBottom: '10px', backgroundColor: '#024731', fontfamily: 'Khula' };
     /*  const backColor = { color: 'green' };
     color={backColor}
     */
@@ -19,8 +19,10 @@ class NavBar extends React.Component {
           <Header inverted as='h1'>Musicians of Manoa</Header>
         </Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Profile</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Filter By</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Jam Sessions</Menu.Item>,
+            ]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>

7deb7f38d93d79c54422cb5fef252e261b232032
Leighton villanueva <lavillan@hawaii.edu>
2018-12-02 12:56:43 -1000

    Removed extra return statement

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -8,39 +8,6 @@ import { Menu, Dropdown } from 'semantic-ui-react';
 class Footer extends React.Component {
   render() {
     const menuStyle = { backgroundColor: '#024731', fontfamily: 'Khula', color: '#FFFFFF' };
-    // return (
-    //     <footer>
-    //       <div style={menuStyle}>
-    //         <hr/>
-    //         <Grid container columns={3}>
-    //           <Grid.Column>Main Menu
-    //             <hr/>
-    //             <List>
-    //               <List.Item>Users</List.Item>
-    //               <List.Item>Musical Tastes</List.Item>
-    //               <List.Item>Musical Capabilities</List.Item>
-    //               <List.Item>Musical Goals</List.Item>
-    //             </List>
-    //           </Grid.Column>
-    //           <Grid.Column>Location
-    //             <hr/>
-    //             <List>
-    //               <List.Item>Department of Information and Computer Sciences</List.Item>
-    //               <List.Item>University of Hawaii</List.Item>
-    //               <List.Item>Honolulu, HI 96822</List.Item>
-    //             </List>
-    //           </Grid.Column>
-    //           <Grid.Column>Contact
-    //             <hr/>
-    //             <List>
-    //               <List.Item>808-531-1888</List.Item>
-    //               <List.Item>cmoore@hawaii.edu</List.Item>
-    //             </List>
-    //           </Grid.Column>
-    //         </Grid>
-    //       </div>
-    //     </footer>
-
     return (
         <Menu style={menuStyle} attached="top" borderless inverted>
           <Menu.Item>

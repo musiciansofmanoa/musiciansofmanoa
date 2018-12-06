3baf62829fa27e6b4d5ac53c0e97e1a09c6456e7
Mia N Clause <mnclause@gmail.com>
2018-12-06 10:31:25 -1000

    Updated Profile

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -46,7 +46,7 @@ export default class Signin extends React.Component {
     }
     // Otherwise return the Login form.
     return (
-        <div className='app-background'>
+        <div>
         <Container>
           <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
             <Grid.Column>

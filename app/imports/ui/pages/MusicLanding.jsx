52bae20ea30ad4c8f19abd84149d72a7bfd88c0e
TyUehara <tyu@hawaii.edu>
2018-12-05 20:32:53 -1000

    background done

--- app/imports/ui/pages/MusicLanding.jsx
+++ app/imports/ui/pages/MusicLanding.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Icon, Header, Column, Grid, Image} from 'semantic-ui-react';
+import {Icon, Header, Segment, Grid, Image} from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class MusicLanding extends React.Component {
@@ -16,22 +16,25 @@ class MusicLanding extends React.Component {
                   <Header as='h1' icon>
                     <div className="readable">
                     <Icon inverted color='grey' name='address card outline' circular/>
-                      <Header.Content id="naniTF">
+                      <Header.Content>
                     Create a Profile
                       </Header.Content>
-                    <Header.Subheader>Express who you are, and what you can bring to a band.</Header.Subheader>
+                    <Header.Subheader id="naniTF">Express who you are, and what you can bring to a band.</Header.Subheader>
                     </div>
                   </Header>
 
                   <div className="ui divider"/>
 
-                  <Image
-                      src='https://react.semantic-ui.com/images/wireframe/image-text.png'
-                      as='a'
-                      size='medium'
-                      href='http://google.com'
-                      target='_blank'
-                  />
+                  <Segment id="seg">
+                    <Image src="/images/tempsnip.png" id="borders" size='medium' centered />
+                    <br></br>
+                    <p>
+                      Create a profile and join the community of musicians around the University of Hawaii at Manoa. By creating a profile, you gain recognition as you search for a band.
+                    </p>
+                    <p>
+                      After creating a profile, you may login and plan "Jam Sessions" with other users!
+                    </p>
+                  </Segment>
 
                 </Grid.Column>
               </Grid>
@@ -62,13 +65,16 @@ class MusicLanding extends React.Component {
 
                     <div className="ui divider"/>
 
-                    <Image
-                        src='https://react.semantic-ui.com/images/wireframe/image-text.png'
-                        as='a'
-                        size='medium'
-                        href='http://google.com'
-                        target='_blank'
-                    />
+                    <Segment id="seg">
+                      <Image src="/images/band.jpg" id="borders" size='medium' centered />
+                      <br></br>
+                      <p>
+                        Using your profile you can connect with other musically talented individuals. This can be a great way to feed your musical passion with like minded individuals.
+                      </p>
+                      <p>
+                        Meet others who also attend your school, and create a band while making life long friends.
+                      </p>
+                    </Segment>
 
                   </Grid.Column>
                 </Grid>
@@ -101,13 +107,15 @@ class MusicLanding extends React.Component {
 
                     <div className="ui divider"/>
 
-                    <Image
-                        src='https://react.semantic-ui.com/images/wireframe/image-text.png'
-                        as='a'
-                        size='medium'
-                        href='http://google.com'
-                        target='_blank'
-                    />
+                    <Segment id="seg">
+                      <Image src="/images/uh.png" id="borders" size='medium' centered />
+                      <p>
+                        Without your reviews, we cannot improve your experience. By leaving a review, we can immediately fix problems before they bug you for eternity. Constructive criticism is wanted.
+                      </p>
+                      <p>
+                        Let us know if you met someone who makes you feel uncomfortable. Your safety is a major concern and we would like to address issues like these as quick as possible.
+                      </p>
+                    </Segment>
 
                   </Grid.Column>
                 </Grid>
